5b8fdd7625aa0bc46c2505530d246b5b62064212
Sami Munir <smunir2001@gmail.com>
2023-02-17 10:49:21 -0500

    navbar updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,22 @@
 import React from "react";
 import '../styles/Navbar.styles.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faObjectGroup } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
     return (
-        <>Hello world, this is the navbar component.</>
+        <div className='main-navbar'>
+            <div className="main-navbar-col1">
+                <h1><FontAwesomeIcon icon={faObjectGroup} /> PC Utopia</h1>
+            </div>
+            <div className="main-navbar-col2">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>My Account</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </div>
     );
 }
